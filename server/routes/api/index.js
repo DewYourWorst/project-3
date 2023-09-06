385261f39994e1f957f09076436e7ff3ca6ccec2
cody-and <codya.0807@icloud.com>
2023-09-06 13:59:08 -0500

    team and user seed data works, just need to fix sport seed data

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
-
 const sportRoutes = require('./sport.routes');
 const teamRoutes = require('./team.routes')
 // const sportRoutes = require('./sport');
 // const teamRoutes = require('./team');
 // const userRoutes = require('./user');
-
-
+// These routes do the same job but do not utilize the controllers we set up, keeping these here to give us the option to pivot to this later on if we want to 
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
