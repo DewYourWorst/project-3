8ed7774d6d182a41a4d5cc7d713ef8a84453ae26
cody-and <codya.0807@icloud.com>
2023-09-06 12:55:24 -0500

    fixing routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,21 +4,17 @@ const userRoutes = require('./user.routes');
 
 const sportRoutes = require('./sport.routes');
 const teamRoutes = require('./team.routes')
-=======
-const sportRoutes = require('./sport');
-const teamRoutes = require('./team');
-const userRoutes = require('./user');
+// const sportRoutes = require('./sport');
+// const teamRoutes = require('./team');
+// const userRoutes = require('./user');
 
 
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/sport', sportRoutes);
-
 router.use('/team', teamRoutes)
-=======
-router.use('/team', teamRoutes);
-router.use('/user', userRoutes);
+
 
 
 module.exports = router;
