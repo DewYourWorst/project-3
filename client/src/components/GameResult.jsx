1b5ecd92eef7d6120a9774edc4a57fbc6dd36d9a
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-09-08 10:31:58 -0500

    added functionality to game result comp

--- client/src/components/GameResult.jsx
+++ client/src/components/GameResult.jsx
@@ -6,7 +6,7 @@ import Row from 'react-bootstrap/Row';
 function GameResult(props) {
   let homeboarder = "primary";
   let awayboarder = "primary";
-  if (props.homescore > props.awayscore) {
+  if (props.homescore < props.awayscore) {
     homeboarder = "success"
     awayboarder = "danger"
   } else {
@@ -27,12 +27,12 @@ function GameResult(props) {
           </Card.Text>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Points: {props.homescore}</ListGroup.Item>
           <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
           <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
         </ListGroup>
         <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">View team</Card.Link>
           <Card.Link href="#">Another Link</Card.Link>
         </Card.Body>
       </Card>
@@ -46,12 +46,12 @@ function GameResult(props) {
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Points: {props.awayscore}</ListGroup.Item>
         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
         <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">View team</Card.Link>
         <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
