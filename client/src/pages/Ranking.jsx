7f9a60e3a401b5102ca6470d37ce86e8a488cf74
tridean <tridean97@gmail.com>
2023-09-12 13:34:53 -0500

    initial styling

--- client/src/pages/Ranking.jsx
+++ client/src/pages/Ranking.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Ranking() {
   const [data, setData] = useState(null);
-  const [year, setYear] = useState(2023); 
-  const [season, setSeason] = useState('regular'); 
-  const [week, setWeek] = useState(1); 
-  const [searchYear, setSearchYear] = useState(2023); 
-  const [searchWeek, setSearchWeek] = useState(1); 
+  const [year, setYear] = useState(2023);
+  const [season, setSeason] = useState('regular');
+  const [week, setWeek] = useState(1);
+  const [searchYear, setSearchYear] = useState(2023);
+  const [searchWeek, setSearchWeek] = useState(1);
 
   const fetchData = async () => {
     try {
@@ -43,47 +43,56 @@ function Ranking() {
   };
 
   const handleFetchClick = () => {
-    setYear(searchYear === '' ? 2023 : searchYear); 
-    setWeek(searchWeek === '' ? 1 : searchWeek); 
+    setYear(searchYear === '' ? 2023 : searchYear);
+    setWeek(searchWeek === '' ? 1 : searchWeek);
   };
 
   return (
-    <div>
-      <h2>National Rankings</h2>
+    <div className="w-1/2 mx-auto p-8 bg-gray-800 rounded-lg shadow-lg text-white">
+      <h2 className="text-3xl font-semibold mb-4 text-center">National Rankings</h2>
 
-      <div>
-        <label>Year:</label>
+      <div className="mb-4">
+        <label className="text-gray-400">Year:</label>
         <input
           type="number"
           id="year"
           value={searchYear}
           onChange={handleYearChange}
+          className="w-full py-2 px-3 bg-gray-700 border rounded-lg text-gray-200 focus:outline-none focus:ring focus:border-blue-500"
         />
       </div>
-      <div>
-        <label>Week:</label>
+      <div className="mb-4">
+        <label className="text-gray-400">Week:</label>
         <input
           type="number"
           id="week"
           value={searchWeek}
           onChange={handleWeekChange}
+          className="w-full py-2 px-3 bg-gray-700 border rounded-lg text-gray-200 focus:outline-none focus:ring focus:border-blue-500"
         />
       </div>
 
-      <button onClick={handleFetchClick}>Search!</button>
+      <button
+        onClick={handleFetchClick}
+        className="bg-blue-500 hover:bg-blue-600 text-black font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring focus:ring-blue-300 w-full"
+      >
+        Search!
+      </button>
 
       {data &&
         data[0].polls
           .filter((poll) => poll.poll === 'AP Top 25' || poll.poll === 'Coaches Poll')
           .map((poll) => (
-            <div key={poll.poll}>
-              <h3>{poll.poll}</h3>
-              <ul>
+            <div key={poll.poll} className="mt-8">
+              <h3 className="text-2xl font-semibold">{poll.poll}</h3>
+              <ul className="mt-4">
                 {poll.ranks.map((rank) => (
-                  <li key={rank.school}>
+                  <li key={rank.school} className="mb-2">
                     <div>
                       <p>
-                        <Link to={`/team/${rank.school}`}>{rank.school}</Link>
+                        <Link to={`/team/${rank.school}`} className="text-blue-500 hover:underline">
+                          {rank.school}
+                        </Link>
                       </p>
                     </div>
                   </li>
