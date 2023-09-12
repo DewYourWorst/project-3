96e32623e5bef2093e163b83f4f32329bdd91229
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-09-12 11:36:07 -0500

    win loss shows on card with border color

--- client/src/components/GameResult.jsx
+++ client/src/components/GameResult.jsx
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 function GameResult(props) {
-  let homeboarder = "primary";
-  let awayboarder = "primary";
-  if (props.homescore < props.awayscore) {
-    homeboarder = "success"
-    awayboarder = "danger"
-  } else {
-    homeboarder = "danger"
-    awayboarder = "success"
-  }
-
+  
   const [data, setData] = useState(null);
-
+  
   const fetchData = async () => {
     try {
       const response = await fetch('/cfb-api/new-box-score');
@@ -26,43 +16,47 @@ function GameResult(props) {
       const responseData = await response.json();
       console.log(responseData
         )
-      setData(responseData[0]);
-    } catch (error) {
-      console.error('error fetching data', error.message);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  
-  return (
-    <>
+        setData(responseData[0]);
+      } catch (error) {
+        console.error('error fetching data', error.message);
+      }
+    };
+    useEffect(() => {
+      fetchData();
+    }, []);
+    
+
+return (
+  <>
     <Row xs={1} md={2} className="g-4">
-        {data &&
-          data.teams.map((team) => (
-            <Card className='m-2' border={homeboarder} style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={props.homeimg} />
-        <Card.Body>
-          <Card.Title>{team.school}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>Points: {team.points}</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">View team</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-          ))}
+      {data &&
+        data.teams.map((team, index) => (
+          <Card
+            className='m-2'
+            border={data.teams[0].score < data.teams[1].score ? (index === 0 ? "success" : "danger") : (index === 0 ? "danger" : "success")}
+            style={{ width: '18rem' }}
+            key={index}
+          >
+            <Card.Img variant="top" src={props.homeimg} />
+            <Card.Body>
+              <Card.Title>{team.school}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Points: {team.points}</ListGroup.Item>
+              <ListGroup.Item>Conference: {team.conference}</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">View team</Card.Link>
+              <Card.Link href="#">View conference</Card.Link>
+            </Card.Body>
+          </Card>
+        ))}
     </Row>
   </>
-  );
+);
 }
 
 export default GameResult;
