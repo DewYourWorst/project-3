68b37021fde30a560136e7d2fea70cf2db07264b
cody-and <codya.0807@icloud.com>
2023-09-13 15:22:45 -0500

    finished dashboard page and added favorite team button to conferences page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -32,26 +32,29 @@ const Dashboard = () => {
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <h2>Welcome Back, {fName}</h2>
-      <div>
-        <h2>Your Favorite Teams</h2>
-        <ul>
-          {userTeams.map((team, index) => (
-            <li key={index}>
-              <Link to={`/team/${team}`}>{team}</Link>
-            </li>
-          ))}
-        </ul>
-
-        <h2>Your Favorite Conferences</h2>
-        <ul>
-          {userConferences.map((conference, index) => ( 
-            <li key={index}>
-              <Link to={`/conferences/${conference}`}>{conference}</Link>
-            </li>
-          ))}
-        </ul>
+      <h1 className="text-8xl text-blue-400 font-semibold mb-6 mt-4 text-center">Dashboard</h1>
+      <h2 className="text-5xl text-blue-400 font-semibold mb-4 text-center">Welcome Back, {fName}</h2>
+      <div className="flex">
+        <div className="w-1/2">
+          <h2 className="text-3xl text-blue-400 font-semibold mb-2 text-center">Your Favorited Teams</h2>
+          <ul>
+            {userTeams.map((team, index) => (
+              <li className="bullet text-3xl text-white font-semibold mb-6 underline text-center" key={index}>
+                <Link to={`/team/${team}`}>{team}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-3xl text-blue-400 font-semibold mb-2 text-center">Your Favorited Conferences</h2>
+          <ul>
+            {userConferences.map((conference, index) => ( 
+              <li className="bullet text-3xl text-white font-semibold mb-6 underline text-center" key={index}>
+                <Link to={`/conferences/${conference}`}>{conference}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
